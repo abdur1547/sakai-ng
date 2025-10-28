622e7762bc7ff39e5536a8de2419f81327fb765f
Abdur Rahman <khalil.ahmad1547@gmail.com>
2025-10-28 11:57:58 +0500

    new login component is created.

---
+++ src/app/pages/auth/login/login.ts
@@ -0,0 +1,20 @@
+import { AppFloatingConfigurator } from '@/layout/component/app.floatingconfigurator';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { CheckboxModule } from 'primeng/checkbox';
+import { InputTextModule } from 'primeng/inputtext';
+import { PasswordModule } from 'primeng/password';
+import { RippleModule } from 'primeng/ripple';
+
+@Component({
+  selector: 'app-login',
+  imports: [ButtonModule, CheckboxModule, InputTextModule, PasswordModule, FormsModule, RouterModule, RippleModule, AppFloatingConfigurator],
+  templateUrl: './login.html'
+})
+export class Login {
+  email: string = '';
+  password: string = '';
+  checked: boolean = false;
+}
